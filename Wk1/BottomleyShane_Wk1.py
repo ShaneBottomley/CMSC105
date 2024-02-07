c986377597b2c5313a79b844dc6fe5975bf1e7e0
Shane Bottomley <shanekbottomley00@gmail.com>
2024-02-06 22:03:59 -0500

    This is my week one assignment where I learned to assign variables and print them.

---
+++ Wk1/BottomleyShane_Wk1.py
@@ -0,0 +1,5 @@
+print("Hello, class! My name is Shane, I am in CMSC 105 6391 and it is Jan 4th, 2021. Happy New Years!") #pt1 no variables
+
+myName = "Shane" #pt2 with variables
+myClass = "CMSC 105 6391"
+print("Hello, class! My name is\t", myName, "I am in", myClass, "This is so fun already!")
